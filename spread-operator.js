58645d2d157be92c6568d84fb4409ef65d83c4cd
Peter Mcloughlin <mcloughlin78@gmail.com>
2024-08-15 09:38:40 +0000

    Finished spread-operator tutorial

---
+++ spread-operator.js
@@ -0,0 +1,28 @@
+/**
+ * To run this file in Gitpod, use the 
+ * command node the-spread-operator.js in the terminal
+ */
+
+// No spread operator
+let arr1 = [1,2,3];
+let arr2 = arr1; //Its not a copy of arr1, its actually a new variable that points to the same object as arr1
+arr2.push(4);
+console.log("Second array: ", arr2);
+console.log("First array: ", arr1);
+// Copying an array
+let arr3 = [4,5,6];
+let arr4 = [...arr3]; //this spread operator creates a copy of arr3
+
+arr4.push(7);
+console.log("Third array: ", arr3);
+console.log("Fourth array: ", arr4);
+// Copying an object
+let obj1 = {a: 1, b: 2, c: 3};
+let obj2 = {...obj1, d: 4};
+let obj3 = {...obj1, b: 5}; //overrides value of b
+console.log("First Object: ", obj1);
+console.log("Second Object: ", obj2);
+console.log("Thrid Object: ", obj3);
+// Copying only part of an array/object
+let arr5 = [...arr1, {...obj1}, ...arr3, "x", "y", "z"];
+console.log(arr5);
