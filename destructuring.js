536dcad140126835c4c5fd66475ac17316e83cfe
Peter Mcloughlin <mcloughlin78@gmail.com>
2024-08-15 09:36:51 +0000

    Finished destructuring tutorial

--- destructuring.js
+++ destructuring.js
@@ -1,12 +1,54 @@
 /**
  * To run this file in Gitpod, use the 
- * command node destructuring.js in the terminal
+ * command node rest-parameter-syntax.js in the terminal
  */
 
-// Destructuring arrays
+// Destructuring Arrays
+let ages = [30,26,27];
+// let john = ages[0];
+// let mary = ages[1];
+// let joe = ages[2];
+let [john, mary, joe] = ages;
+console.log(john, mary, joe);
+// Destructuring Objects
+let jobs = {
+    mike: "designer",
+    jill: "developer",
+    alicia: "accountant"
+};
+let {mike, jill, alicia} = jobs;
+console.log(mike, jill, alicia);
 
-// Destructuring objects
+// Destructuring Subsets
+let languages = ["english","french","spanish","german","japanese"];
+let [johnNative, johnSecondary] = languages;
+console.log(johnNative, johnSecondary);
 
-// Destructuring subsets
+let [, , maryNative, marySecondary] = languages;
+console.log(maryNative, marySecondary);
 
+let languages2 = {
+    firstLanguage: "english",
+    secondLanguage: "french",
+    thirdLanguage: "german",
+    fourthLanguage: "japanese"
+}
+let {firstLanguage, thirdLanguage} = languages2;
+console.log(firstLanguage, thirdLanguage);
 // Using rest parameter syntax
+let fruits = ["apple","orange","banana","peach","cherry"];
+let [favourite, secondFavourite, ...others] = fruits;
+console.log(favourite);
+console.log(secondFavourite);
+console.log(others);
+
+let favouriteFoods = {
+    brian: "pizza",
+    anna: "pasta",
+    sarah: "vegetarian",
+    andrea: "steak"
+};
+let {brian, anna, ...rest} = favouriteFoods;
+console.log(brian);
+console.log(anna);
+console.log(rest);
